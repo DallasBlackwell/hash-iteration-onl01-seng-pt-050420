805dd01ac4278d48892e1b1dd691f3dfb0036822
Dallas Blackwell <mdvayne923@gmail.com>
2020-05-15 23:44:49 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,3 +8,8 @@ def happy_birthday(birthday_kids)
   "Happy Birthday #{kids_name}! You are now #{age} years old!"
 end
 
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
